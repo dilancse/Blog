2ba901ec74d58259feb30aba2d7834ccb2cb3293
g_dilan <g.dilan@directfn.com>
2022-03-09 19:48:39 +0530

    Initialize express server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello world")
+import express, { Application, json, urlencoded, Request, Response } from 'express';
+const port = 5000;
+
+const app: Application = express();
+
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello');
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
